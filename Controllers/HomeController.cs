7d3c63309bad580f6fce953b18e8320b0cc95be2
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-20 14:58:53 +1200

    Add HomeController and its Folder

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventRegistrationSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
