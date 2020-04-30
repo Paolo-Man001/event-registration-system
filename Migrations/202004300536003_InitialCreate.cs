a9caf57c8726aa7e1826273c8ffed67097350ef9
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-30 17:43:01 +1200

    Add EF Migrations: InitialCreate. Update-Db and create Tables: Client, Event

---
+++ Migrations/202004300536003_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿namespace EventRegistrationSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Client",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        FullName = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Address = c.String(nullable: false),
+                        Phone = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Event",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ClientID = c.Int(nullable: false),
+                        EventName = c.String(nullable: false),
+                        Description = c.String(),
+                        Location = c.String(nullable: false),
+                        Date = c.DateTime(nullable: false),
+                        TicketPrice = c.Decimal(nullable: false, precision: 18, scale: 2),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.Client", t => t.ClientID, cascadeDelete: true)
+                .Index(t => t.ClientID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Event", "ClientID", "dbo.Client");
+            DropIndex("dbo.Event", new[] { "ClientID" });
+            DropTable("dbo.Event");
+            DropTable("dbo.Client");
+        }
+    }
+}
---
+++ Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace EventRegistrationSystem.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<EventRegistrationSystem.DAL.EventRegistrationContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(EventRegistrationSystem.DAL.EventRegistrationContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
