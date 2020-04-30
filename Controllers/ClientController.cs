0d79652759ed54dc393dea944b9457466c8bb252
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-30 18:35:46 +1200

    Remove DataInitializer file and Web.config Initializer-setting/element. Transfer All seed Code-blocks to Configuration.cs in Migrations-folder to seed data.

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,20 @@
+﻿using EventRegistrationSystem.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EventRegistrationSystem.Controllers
+{
+    public class ClientController : Controller
+    {
+        private EventRegistrationContext db = new EventRegistrationContext();
+
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View(db.Clients.ToList());
+        }
+    }
+}
