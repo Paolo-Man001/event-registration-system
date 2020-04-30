13714bd775991f713639e7de346f29c2845c0044
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-04-30 12:07:23 +1200

    Add Client and Event Models. Add Initial Annotation. Add reference to fk-model

---
+++ Models/Client.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventRegistrationSystem.Models
+{
+    public class Client
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+        
+        [Required]
+        public string Address { get; set; }
+        
+        [Required]
+        public string Phone { get; set; }
+
+
+        public virtual ICollection<Event> Events { get; set; }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EventRegistrationSystem.Models
+{
+    public class Event
+    {
+        public int ID { get; set; }
+        public int ClientID { get; set; }
+
+        [Required]
+        public string EventName { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public decimal TicketPrize { get; set; }
+
+
+        public virtual Client Client { get; set; }
+    }
+}
