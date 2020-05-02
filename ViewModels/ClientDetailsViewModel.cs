16dcce270eb97a788d4bb983bcbb1669ae60bd4a
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-02 15:27:06 +1200

    Add ViewModels Folder. Create ClientDetailsViewModel.cs

---
+++ ViewModels/ClientDetailsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EventRegistrationSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventRegistrationSystem.ViewModels
+{
+    public class ClientDetailsViewModel
+    {
+        public IEnumerable<Client> Clients { get; set; }
+        public IEnumerable<Event> Events { get; set; }
+    }
+}
