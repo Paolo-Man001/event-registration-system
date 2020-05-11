303bd0540c09b637c9d3d69693e16ab5b90af723
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-11 23:07:59 +1200

    Add all properties for Booking.cs Model. Add EventId as FK. Add Event-Ref-Model.

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,29 @@ namespace EventRegistrationSystem.Models
     {
         public int ID { get; set; }
         public int EventID { get; set; }           // Foreign Key
+        public virtual Event Event { get; set; }    // Model for FK
+
+        [Required(ErrorMessage = "Name is required.")]
+        [Display(Name = "Customer Name")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "Name must be at 2 to 100 characters.")]
+        public string CustomerName { get; set; }
+
+        [Required(ErrorMessage = "Customer Email is required.")]
+        [EmailAddress, Display(Name = "Customer Email")]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Email must be 6 to 100 characters.")]
+        public string CustomerEmail { get; set; }
+
+        [Required(ErrorMessage = "Customer Phone Number is required.")]
+        [Display(Name = "Customer Phone")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Phone number must be 6 to 50 Characters.")]
+        public string CustomerPhone { get; set; }
+
+        [Required(ErrorMessage = "1 ticket per person is required for this event.")]
+        [Display(Name = "Ticket Count")]
+        public int TicketCount { get; set; }
+
+        [Display(Name = "Total Price"), DataType(DataType.Currency)]
+        public decimal TotalPurchase { get; set; }
 
     }
 }
