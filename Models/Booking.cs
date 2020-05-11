bc92a6a36544aba9f8345b37656bedee1ee890ce
Paolo Angelo <39821263+Paolo-Man001@users.noreply.github.com>
2020-05-11 22:41:18 +1200

    Add Booking.cs Model.

---
+++ Models/Booking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventRegistrationSystem.Models
+{
+    public class Booking
+    {
+        public int ID { get; set; }
+        public int EventID { get; set; }           // Foreign Key
+
+    }
+}
